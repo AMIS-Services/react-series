54aa5060d6f9a8acc86548ff06b9252773425459
bramkaashoek <bram.kaashoek@amis.nl>
2018-08-24 12:37:30 +0200

    fix cors

--- sig2/example/koa/app.js
+++ sig2/example/koa/app.js
@@ -12,8 +12,12 @@ mongoose.connect(
 );
 
 app.use(bodyParser());
-app.use(function*() {
-  this.set("Access-Control-Allow-Origin", "*");
+
+app.use(async (ctx, next) => {
+  ctx.set("Access-Control-Allow-Origin", "*");
+  ctx.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  ctx.set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  await next();
 });
 
 app.get("/", async ctx => {
