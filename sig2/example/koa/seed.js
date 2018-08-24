f04a323fac354ce5ef41f7f44fed81de0eada222
bramkaashoek <bram.kaashoek@amis.nl>
2018-08-24 11:43:04 +0200

    add error logging

--- sig2/example/koa/seed.js
+++ sig2/example/koa/seed.js
@@ -1,65 +1,62 @@
-const request = require('request');
+const request = require("request");
 
 const accommodations = [
   {
     id: 1,
-    name: 'Romantische vuurtoren',
-    location: 'Scheveningen',
+    name: "Romantische vuurtoren",
+    location: "Scheveningen",
     favorite: false,
-    image:
-      'http://www.weekendhotel.nl/acco_images/0002/0670/Vuurtoren_en_zeilboot.jpg',
+    image: "http://www.weekendhotel.nl/acco_images/0002/0670/Vuurtoren_en_zeilboot.jpg",
     description:
-      'Een zeer romantische lokatie aan de zee. Verblijf in een authentieke vuurtoren en overzie vanaf grote hoogte de zee. Enkel voor gasten met een goede conditie.',
+      "Een zeer romantische lokatie aan de zee. Verblijf in een authentieke vuurtoren en overzie vanaf grote hoogte de zee. Enkel voor gasten met een goede conditie."
   },
   {
     id: 2,
-    name: 'Bezemkast in Amsterdam',
-    location: 'Amsterdam',
+    name: "Bezemkast in Amsterdam",
+    location: "Amsterdam",
     favorite: false,
-    image:
-      'https://i.pinimg.com/originals/db/2b/20/db2b207c1bb61e5b7d1bafe1c81455ff.jpg',
+    image: "https://i.pinimg.com/originals/db/2b/20/db2b207c1bb61e5b7d1bafe1c81455ff.jpg",
     description:
-      'De beste woning die je in Amsterdam gaat vinden onder de 500 euro per nacht. Accepteer de realiteit van de woningnood en zie de positieve kant in: het is erg knus.',
+      "De beste woning die je in Amsterdam gaat vinden onder de 500 euro per nacht. Accepteer de realiteit van de woningnood en zie de positieve kant in: het is erg knus."
   },
   {
     id: 3,
-    name: 'Hutje op de hei',
-    location: 'De Veluwe',
+    name: "Hutje op de hei",
+    location: "De Veluwe",
     favorite: false,
-    image:
-      'http://static.zoom.nl/FE47E471D162973435638C27B1A35BB6-hutje-op-de-hei.jpg',
+    image: "http://static.zoom.nl/FE47E471D162973435638C27B1A35BB6-hutje-op-de-hei.jpg",
     description:
-      'Wat is nou een klassiekere Nederlands verblijfsplaats dan een hutje op de hei? Als je even weg wil van de drukte en belachelijkhe huizenprijzen van de stad is dit een perfecte locatie',
+      "Wat is nou een klassiekere Nederlands verblijfsplaats dan een hutje op de hei? Als je even weg wil van de drukte en belachelijkhe huizenprijzen van de stad is dit een perfecte locatie"
   },
   {
     id: 4,
-    name: 'Vakantievilla Vacuna',
-    location: 'Domburg',
+    name: "Vakantievilla Vacuna",
+    location: "Domburg",
     favorite: true,
-    image: 'http://www.zeelandrelais.com/files/DSC00204.jpg',
+    image: "http://www.zeelandrelais.com/files/DSC00204.jpg",
     description:
-      'Een rustieke vakantievilla gelegen in Domburg aan de zeeuwse kust. Uitstekende beheersing van de Duitse taal is helaas wel een vereist voor communicatie in de omgeving.',
+      "Een rustieke vakantievilla gelegen in Domburg aan de zeeuwse kust. Uitstekende beheersing van de Duitse taal is helaas wel een vereist voor communicatie in de omgeving."
   },
   {
     id: 5,
-    name: 'Appartement voor twee personen',
-    location: 'Utrecht',
+    name: "Appartement voor twee personen",
+    location: "Utrecht",
     favorite: false,
-    image:
-      'http://www.interieur-inrichting.net/afbeeldingen/knus-scandinavisch-appartement-645x484.jpg',
+    image: "http://www.interieur-inrichting.net/afbeeldingen/knus-scandinavisch-appartement-645x484.jpg",
     description:
-      'Een appartement voor twee personen in Utrecht. Ingericht in typische vtwonen stijl. Mooi op het plaatje maar in de realiteit veel te wit.',
-  },
+      "Een appartement voor twee personen in Utrecht. Ingericht in typische vtwonen stijl. Mooi op het plaatje maar in de realiteit veel te wit."
+  }
 ];
 
 accommodations.map(acc => {
   const options = {
-    url: 'http://localhost:3030/accommodations',
-    headers: { 'Content-Type': 'application/json' },
-    method: 'POST',
-    body: JSON.stringify(acc),
+    url: "http://localhost:3030/accommodations",
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+    body: JSON.stringify(acc)
   };
   request(options, (err, res, body) => {
-    console.log('accommodation seeded');
+    if (err) console.log(err);
+    console.log("accommodation seeded");
   });
 });
