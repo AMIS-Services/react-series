78e9b8ea6c61135a4b309e68618b5e94da08ee95
Nathan Breuring <nathan.breuring@amis.nl>
2018-11-08 13:12:29 +0100

    Styling inlog dialog

--- sig3/example/react/src/components/Login/LoginDialog.js
+++ sig3/example/react/src/components/Login/LoginDialog.js
@@ -9,6 +9,19 @@ const styles = {
       fontWeight: "bold",
     },
   },
+  userFields: {
+    display: "flex",
+    flexDirection: "column",
+    margin: 16,
+    "& div:nth-child(2n)": {
+      marginTop: 16,
+    },
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "flex-end",
+    margin: 16,
+  },
 };
 
 class LoginDialog extends React.Component {
@@ -21,12 +34,18 @@ class LoginDialog extends React.Component {
     return (
       <Dialog open={this.props.open}>
         <DialogTitle className={this.props.classes.title}>Login</DialogTitle>
-        <div>
-          Username:
-          <Input />
-          Password:
-          <Input />
+        <div className={this.props.classes.userFields}>
+          <div>
+            Username:
+            <Input />
+          </div>
+          <div>
+            Password:
+            <Input />
+          </div>
           {/* TODO: Register */}
+        </div>
+        <div className={this.props.classes.buttons}>
           <Button onClick={this.handleLogin}>OK</Button>
           <Button onClick={this.props.handleClose}>Cancel</Button>
         </div>
