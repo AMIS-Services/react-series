a29cd67a39d89dfa8a181a20b32b4b27b5a7501e
Nathan Breuring <nathan.breuring@amis.nl>
2018-11-16 16:01:02 +0100

    Login after register using context api

--- sig3/example/react/src/components/Login/Register.js
+++ sig3/example/react/src/components/Login/Register.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { TextField, Button } from "@material-ui/core";
+import { UserContext } from "../../common/context";
 import { fetch } from "../../common/fetch";
 
 const styles = {
@@ -33,44 +34,51 @@ class Register extends React.Component {
   submit = async () => {
     const { ...user } = this.state;
     await fetch("users", { method: "POST", body: user });
+    this.context.login(this.state.email, this.state.password);
     this.props.history.push("/");
   };
 
   render() {
     return (
-      <div className={this.props.classes.root}>
-        <form className={this.props.classes.inputs}>
-          <TextField
-            id="username"
-            required
-            label="Gebruikersnaam"
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-          <TextField
-            id="password"
-            type="password"
-            required
-            label="Wachtwoord"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-          <TextField
-            id="email"
-            required
-            label="Email"
-            type="email"
-            autoComplete="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-        </form>
-        <div className={this.props.classes.buttons}>
-          <Button onClick={this.submit}>OK</Button>
-        </div>
-      </div>
+      <UserContext.Consumer>
+        {context => (
+          <div className={this.props.classes.root}>
+            <form className={this.props.classes.inputs}>
+              <TextField
+                id="username"
+                required
+                label="Gebruikersnaam"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+              <TextField
+                id="password"
+                type="password"
+                required
+                label="Wachtwoord"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+              <TextField
+                id="email"
+                required
+                label="Email"
+                type="email"
+                autoComplete="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </form>
+            <div className={this.props.classes.buttons}>
+              <Button onClick={this.submit}>OK</Button>
+            </div>
+          </div>
+        )}
+      </UserContext.Consumer>
     );
   }
 }
+
+Register.contextType = UserContext;
 
 export default withStyles(styles)(Register);
